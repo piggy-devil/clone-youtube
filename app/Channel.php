93eb5a8e09ca4c2747f27923effda417094d2483
piggy-devil <muupaza@gmail.com>
2020-02-03 14:33:23 +0700

    lesson-6-authorizing-channel-updates

--- app/Channel.php
+++ app/Channel.php
@@ -11,20 +11,53 @@ class Channel extends Model implements HasMedia
 {
     use HasMediaTrait;
 
+    /**
+     * A channel belongs to a user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     *
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     *
+     * Return the image for this channel
+     *
+     * @return string|null
+     *
+     */
     public function image()
     {
         if ($this->media->first()) {
-            return $this->media->first()->getFullUrl('thumb');
+            return $this->media->first()->getFullUrl('thumb');   // Can Use ->getUrl('thumb')
         }
 
         return null;
     }
 
+    /**
+     *
+     * Check if a user is authorized to edit this model instance
+     *
+     * @return boolean
+     *
+     */
+    public function editable()
+    {
+        if (! auth()-> check()) return false;
+
+        return $this->user_id === auth()->user()->id;
+    }
+
+    /**
+     * Register the media conversions.
+     *
+     * @return null
+     *
+     */
     public function registerMediaConversions(?Media $media = null)
     {
         $this->addMediaConversion('thumb')
