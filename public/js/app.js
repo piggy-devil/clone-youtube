08c1144dbcd29538f444aaf0134315c12a88cb00
piggy-devil <muupaza@gmail.com>
2020-02-04 14:32:30 +0700

    lesson-17-upload-progress

--- public/js/app.js
+++ public/js/app.js
@@ -33246,7 +33246,8 @@ Vue.component('channel-uploads', {
   data: function data() {
     return {
       selected: false,
-      videos: []
+      videos: [],
+      progress: {}
     };
   },
   methods: {
@@ -33257,9 +33258,16 @@ Vue.component('channel-uploads', {
       this.videos = Array.from(this.$refs.videos.files);
       var uploaders = this.videos.map(function (video) {
         var form = new FormData();
+        _this.progress[video.name] = 0;
         form.append('video', video);
         form.append('title', video.name);
-        return axios.post("/channels/".concat(_this.channel.id, "/videos"), form);
+        return axios.post("/channels/".concat(_this.channel.id, "/videos"), form, {
+          onUploadProgress: function onUploadProgress(event) {
+            _this.progress[video.name] = Math.ceil(event.loaded / event.total * 100);
+
+            _this.$forceUpdate();
+          }
+        });
       });
     }
   }
