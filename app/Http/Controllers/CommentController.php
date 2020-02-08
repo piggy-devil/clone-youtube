e2fa093ec49a5c130f6d6a42f101bc45cb187f25
piggy-devil <muupaza@gmail.com>
2020-02-08 15:17:26 +0700

    lesson-33-displaying-paginated-replies

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Video;
+use App\Comment;
+use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     public function index(Video $video)
     {
         return $video->comments()->paginate(10);
+    }
+
+    public function show(Comment $comment)
+    {
+        return $comment->replies()->paginate(10);
     }
 }
