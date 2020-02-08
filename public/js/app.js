354f56cea0858d001b2c52feff8679a25f27d2c2
piggy-devil <muupaza@gmail.com>
2020-02-08 16:31:11 +0700

    lesson-37-adding-replies

--- public/js/app.js
+++ public/js/app.js
@@ -1952,6 +1952,7 @@ __webpack_require__.r(__webpack_exports__);
   },
   data: function data() {
     return {
+      body: '',
       addingReply: false
     };
   },
@@ -1961,6 +1962,29 @@ __webpack_require__.r(__webpack_exports__);
       "default": function _default() {
         return {};
       }
+    },
+    video: {
+      required: true,
+      "default": function _default() {
+        return {};
+      }
+    }
+  },
+  methods: {
+    addReply: function addReply() {
+      var _this = this;
+
+      if (!this.body) return;
+      axios.post("/comments/".concat(this.video.id), {
+        comment_id: this.comment.id,
+        body: this.body
+      }).then(function (_ref) {
+        var data = _ref.data;
+        _this.body = '';
+        _this.addingReply = false;
+
+        _this.$refs.replies.addReply(data);
+      });
     }
   }
 });
@@ -2133,6 +2157,11 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
         _this.replies = _objectSpread({}, data, {
           data: [].concat(_toConsumableArray(_this.replies.data), _toConsumableArray(data.data))
         });
+      });
+    },
+    addReply: function addReply(reply) {
+      this.replies = _objectSpread({}, this.replies, {
+        data: [reply].concat(_toConsumableArray(this.replies.data))
       });
     }
   }
@@ -21756,15 +21785,39 @@ var render = function() {
           _vm.addingReply
             ? _c("div", { staticClass: "form-inline my-4 w-full" }, [
                 _c("input", {
+                  directives: [
+                    {
+                      name: "model",
+                      rawName: "v-model",
+                      value: _vm.body,
+                      expression: "body"
+                    }
+                  ],
                   staticClass: "form-control form-control-sm w-80",
-                  attrs: { type: "text" }
+                  attrs: { type: "text" },
+                  domProps: { value: _vm.body },
+                  on: {
+                    input: function($event) {
+                      if ($event.target.composing) {
+                        return
+                      }
+                      _vm.body = $event.target.value
+                    }
+                  }
                 }),
                 _vm._v(" "),
-                _vm._m(0)
+                _c(
+                  "button",
+                  {
+                    staticClass: "btn btn-sm btn-primary",
+                    on: { click: _vm.addReply }
+                  },
+                  [_c("small", [_vm._v("Add reply")])]
+                )
               ])
             : _vm._e(),
           _vm._v(" "),
-          _c("replies", { attrs: { comment: _vm.comment } })
+          _c("replies", { ref: "replies", attrs: { comment: _vm.comment } })
         ],
         1
       )
@@ -21772,16 +21825,7 @@ var render = function() {
     1
   )
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("button", { staticClass: "btn btn-sm btn-primary" }, [
-      _c("small", [_vm._v("Add reply")])
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -21843,7 +21887,10 @@ var render = function() {
         : _vm._e(),
       _vm._v(" "),
       _vm._l(_vm.comments.data, function(comment) {
-        return _c("Comment", { key: comment.id, attrs: { comment: comment } })
+        return _c("Comment", {
+          key: comment.id,
+          attrs: { comment: comment, video: _vm.video }
+        })
       }),
       _vm._v(" "),
       _c("div", { staticClass: "text-center" }, [
