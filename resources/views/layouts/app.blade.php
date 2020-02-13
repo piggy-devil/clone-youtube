c8ea8fefc6ff6f61d217f6d403630ce957a70d46
piggy-devil <muupaza@gmail.com>
2020-02-13 15:45:50 +0700

    layout app design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
 </head>
 <body>
-    <div id="app" class="bg-gray-400">
+    <div id="app" class="bg-gray-200 text-gray-800 antialiased font-sans">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
