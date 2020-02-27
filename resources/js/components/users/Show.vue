bacf348d349f428bc5c25acbf3789eaae9c65ab1
piggy-devil <muupaza@gmail.com>
2020-02-27 15:17:05 +0700

    image resizing on the fly

--- resources/js/components/users/Show.vue
+++ resources/js/components/users/Show.vue
@@ -3,8 +3,8 @@
         <div class="relative mb-8">
             <div class="w-100 h-64 overflow-hidden z-10">
                 <!-- <img src="https://cdn.pixabay.com/photo/2017/03/26/12/13/countryside-2175353_960_720.jpg" alt="user background image" class="object-cover w-full"> -->
-                <UploadableImage image-width="1500"
-                                 image-height="300"
+                <UploadableImage image-width="1200"
+                                 image-height="500"
                                  location="cover"
                                  alt="user background image"
                                  classes="object-cover w-full"
@@ -14,8 +14,8 @@
             <div class="relative flex items-center bottom-0 left-0 -mb-8 ml-12 -mt-20 z-20">
                 <div class="w-32">
                     <!-- <img src="https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg" alt="user profile image" class="object-cover w-32 h-32 border-4 border-gray-200 rounded-full shadow-lg"> -->
-                    <UploadableImage image-width="1500"
-                                     image-height="300"
+                    <UploadableImage image-width="750"
+                                     image-height="750"
                                      location="profile"
                                      alt="user profile image"
                                      classes="object-cover w-32 h-32 border-4 border-gray-200 rounded-full shadow-lg"
